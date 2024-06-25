74be4b4a1dfed74284c3ce725a846b66ebf92371
Eliran Shemesh <piro27@gmail.com>
2024-06-25 19:26:30 +0300

    Focus on the important

--- src/components/Measurement.jsx
+++ src/components/Measurement.jsx
@@ -7,7 +7,7 @@ export const Measurement = ({name, value, range = []}) => {
             "text-red-500": value < range[0] || value > range[1],
             "text-green-500": value >= range[0] && value <= range[1],
         })}>
-            <h2 className="text-xl">{Math.round(value)}</h2>
+            <h2 className="text-3xl">{Math.round(value)}</h2>
             <label className="text-sm">{name}</label>
         </div>
     )

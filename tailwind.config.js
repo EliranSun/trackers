f50b45db9fa567c4f5b36930772418a468c24a8c
Eliran Shemesh <piro27@gmail.com>
2024-06-25 10:40:03 +0300

    Match colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,13 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+        backgroundColor: {
+            gray: {
+                900: "#282c34"
+                }
+            }
+    },
   },
   plugins: [],
 }

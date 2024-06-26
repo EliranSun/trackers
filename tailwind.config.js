cd60147c46c3ef6a4077e62e9626513b1f644772
Eliran Shemesh <piro27@gmail.com>
2024-06-26 09:10:15 +0300

    Fix tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,13 +8,13 @@ module.exports = {
         backgroundColor: {
             gray: {
                 900: "#282c34"
-                }
-            },
+            }
             "blue-sapphire": "#05668D",
             "metalic-seaweed": "#028090",
             "persian-green": "#00A896",
             "mountain-meadow": "#02C39A",
             "pale-spring-bud": "#F0F3BD"
+        }
     },
   },
   plugins: [],

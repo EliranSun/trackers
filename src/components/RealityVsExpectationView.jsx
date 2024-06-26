a14fc29bd875cc30b316bf0bc41bb6742b837bbe
Eliran Shemesh <piro27@gmail.com>
2024-06-26 10:30:44 +0300

    Fix alignment

--- src/components/RealityVsExpectationView.jsx
+++ src/components/RealityVsExpectationView.jsx
@@ -23,20 +23,16 @@ export const RealityVsExpectationView = (props) => {
         <section className="flex flex-col w-screen gap-1">
             {hours.map(hour => (
             <div className="flex gap-1 w-full">
-            <div className="p-2 bg-white/10">
-                <div key={hour}>{hour}</div>
+            <div className="w-1/4 p-2 bg-white/10">
+                {hour}
             </div>
             <div className="flex flex-col gap-px">
-            <div className="">
                     <div key={`${hour}-expectation`} className="bg-white/10">
                         <input type="text" className="w-full h-full bg-transparent"/>
                     </div>
-            </div>
-            <div className="">
                     <div key={`${hour}-reality`} className="bg-white/10">
                         <input type="text" className="w-full h-full bg-transparent"/>
                     </div>
-            </div>
             </div>
             <div className="p-2 bg-white/10">
                 <div key={`${hour}-match}`}>

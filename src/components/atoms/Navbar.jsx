6e4eace5f3da54151b9b7a05bf42d63fa9e80eb8
Eliran Shemesh <piro27@gmail.com>
2024-06-27 17:49:37 +0300

    big UI, easier UX

--- src/components/atoms/Navbar.jsx
+++ src/components/atoms/Navbar.jsx
@@ -2,32 +2,37 @@ import { TrackerIcons, TrackerNames } from "../../constants";
 import classNames from "classnames";
 import { ArrowClockwise } from "@phosphor-icons/react";
 
+const NavButton = ({ children, isSelected, onClick }) => {
+  return (
+    <button
+      className={classNames({
+        "text-white": isSelected,
+        "text-white/50": !isSelected,
+        "p-4": true,
+      })}
+      onClick={onClick}>
+      {children}
+    </button>
+  )
+}
 export const Navbar = ({ selectedView, setSelectedView }) => {
   return (
-    <div className="flex w-screen left-0 fixed bottom-0 h-24 bg-gray-900 justify-evenly px-4 pb-4 font-mono">
+    <div className="flex w-screen left-0 fixed bottom-0 h-24 bg-gray-700 justify-evenly pb-4 font-mono">
       {Object.values(TrackerNames).map((name, index) => {
         const Icon = TrackerIcons[name];
         
         return (
-          <button
+          <NavButton
             key={name}
-            className={classNames({
-              "bg-black": selectedView === name,
-              "bg-transparent": selectedView !== name,
-              "p-4 text-white": true,
-            })}
+            isSelected={selectedView === name}
             onClick={() => setSelectedView(name)}>
-            <Icon/>
-          </button>
+            <Icon size={32}/>
+          </NavButton>
         );
       })}
-      <button
-        onClick={() => window.location.reload()}
-        className={classNames({
-          "p-4 text-white": true,
-        })}>
-        <ArrowClockwise/>
-      </button>
+      <NavButton onClick={() => window.location.reload()}>
+        <ArrowClockwise size={32}/>
+      </NavButton>
     </div>
   );
 };

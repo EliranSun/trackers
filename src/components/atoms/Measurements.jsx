474da24f3630613ccaf3445716f6f5e18b4922be
Eliran Shemesh <piro27@gmail.com>
2024-06-28 12:07:05 +0300

    Adjust for all pages

--- src/components/atoms/Measurements.jsx
+++ src/components/atoms/Measurements.jsx
@@ -6,7 +6,7 @@ export const Measurements = ({ children }) => {
       "m-auto": true,
       "backdrop-blur rounded-full bg-gray-700 shadow-xl": true,
       "w-fit h-20 flex items-center gap-10 py-4 px-10": true,
-      "sticky top-32 inset-x-0 ": true,
+      "sticky top-28 z-10 inset-x-0 ": true,
     })}>
       {children}
     </div>

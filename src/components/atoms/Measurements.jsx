df20f4c976005664b8adbe9d9837154a0fa721c6
Eliran Shemesh <piro27@gmail.com>
2024-06-28 09:03:45 +0300

    See measurements at all times

--- src/components/atoms/Measurements.jsx
+++ src/components/atoms/Measurements.jsx
@@ -6,7 +6,7 @@ export const Measurements = ({ children }) => {
       "m-auto": true,
       "backdrop-blur rounded-full bg-gray-700 shadow-xl": true,
       "w-fit h-20 flex items-center gap-10 py-4 px-10": true,
-      "fixed bottom-32 inset-x-0 ": false,
+      "sticky top-32 inset-x-0 ": true,
     })}>
       {children}
     </div>

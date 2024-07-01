28578d4db43376349dbb3a70e985718160199d79
Eliran Shemesh <piro27@gmail.com>
2024-07-01 17:08:14 +0300

    Update Navbar.jsx

--- src/components/atoms/Navbar.jsx
+++ src/components/atoms/Navbar.jsx
@@ -59,8 +59,8 @@ export const Navbar = ({ selectedView, setSelectedView }) => {
         <div
           onClick={() => setIsMenuOpen(false)}
           className={classNames({
-            "text-black": true,
-            "flex flex-col gap-20 items-end justify-center backdrop-blur-lg": true,
+            "text-black px-8": true,
+            "flex flex-col gap-16 items-end justify-center backdrop-blur-lg": true,
             "w-screen h-screen bg-white/20 fixed inset-0 z-20 ": true,
           })}>
           <NavButton

c9efc576f3fef5501c5cb7aa44fa7dd9f5f6b77a
Eliran Shemesh <piro27@gmail.com>
2024-07-09 22:17:37 +0300

    Update NavButton.jsx

--- src/components/atoms/NavButton.jsx
+++ src/components/atoms/NavButton.jsx
@@ -1,12 +1,13 @@
 import classNames from "classnames";
 
-export const NavButton = ({ children, isSelected, onClick }) => {
+export const NavButton = ({ color, children, isSelected, onClick }) => {
   return (
     <button
       onClick={onClick}
       className={classNames({
+        "text-black": color === "black",
         "text-white": isSelected,
-        "text-white/50": !isSelected,
+        "text-white/50": !isSelected && color !== "black",
         "hover:bg-black user-select-none": true,
         "p-4 relative z-20": true,
         "flex flex-col items-center justify-center": true,

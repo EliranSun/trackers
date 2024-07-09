96f1e2035d12dc9046ecbf7560e6cfae94d4c36d
Eliran Shemesh <piro27@gmail.com>
2024-07-09 07:55:10 +0300

    Update Navbar.jsx

--- src/components/atoms/Navbar.jsx
+++ src/components/atoms/Navbar.jsx
@@ -17,7 +17,7 @@ const NavButton = ({ children, isSelected, onClick }) => {
       className={classNames({
         "text-white": isSelected,
         "text-white/50": !isSelected,
-        "hover:bg-black": true,
+        "hover:bg-black user-select-none": true,
         "p-4 relative z-20": true,
       })}>
       {children}

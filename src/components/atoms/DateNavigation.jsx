96101a20de521746f11cfc455846181fa5e722e5
Eliran Shemesh <piro27@gmail.com>
2024-07-26 10:54:50 +0300

    Color scheme

--- src/components/atoms/DateNavigation.jsx
+++ src/components/atoms/DateNavigation.jsx
@@ -1,11 +1,15 @@
 import {ONE_DAY} from "../../constants";
 import {ArrowSquareLeft, ArrowSquareRight} from "@phosphor-icons/react";
 import {format} from "date-fns";
+import classNames from "classnames";
 
 export const DateNavigation = ({dateObject, setDateObject, dateLabel, setDateLabel}) => {
     return (
         <div
-            className="fixed top-0 z-10 bg-gray-400 dark:bg-gray-900 h-16 left-0 w-screen flex justify-evenly items-center">
+            className={classNames({
+                "fixed top-0 z-10 bg-gray-50 dark:bg-gray-900 h-16 left-0 w-screen flex justify-evenly items-center": true,
+                "text-black dark:text-white": true,
+            })}>
             <button
                 className="text-xl p-4 w-1/3 flex justify-center items-center"
                 onClick={() => {

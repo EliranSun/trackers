673f13a0bd99b29ead4fc1a50228bedd3f206dc3
Eliran Shemesh <piro27@gmail.com>
2024-07-31 19:42:09 +0300

    Update Measurements.jsx

--- src/components/atoms/Measurements.jsx
+++ src/components/atoms/Measurements.jsx
@@ -4,7 +4,7 @@ export const Measurements = ({children}) => {
     return (
         <div className={classNames({
             "m-auto": true,
-            "backdrop-blur rounded-full bg-gray-700 shadow-xl": true,
+            "backdrop-blur rounded-full bg-gray-200 dark:bg-gray-700 shadow-xl": true,
             "w-fit flex items-center gap-10 py-3 px-10": true,
             "sticky top-20 z-10 inset-x-0": true,
         })}>
